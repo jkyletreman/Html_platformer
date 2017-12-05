75615bb214b7b875d1f53541db56cfe99e33c809
Kyle Treman <jkt290@nyu.edu>
2017-12-05 13:51:27 -0500

    player left-right

--- game.js
+++ game.js
@@ -1,21 +1,45 @@
+//Variables
+
 var gameBoard = document.getElementById("canvas"),
     grphix = canvas.getContext("2d"),
     width = 400,
     height = 800;
 
-// Player Img
 var player = new Object('Jungle Asset Pack/Character/sprites/idle.gif', (width/2), (height - 50));
+var isLeft = false;
+var isRight = false;
+// Events
+
+function keyDown(e) {
+  if (String.fromCharCode(e.keyCode) === '%') isLeft = true;
+  if (String.fromCharCode(e.keyCode) === "'") isRight = true;
+}
+
+function keyUp(e) {
+  if (String.fromCharCode(e.keyCode) === '%') isLeft = false;
+  if (String.fromCharCode(e.keyCode) === "'") isRight = false;
+}
 
 
 
 // Main Loop
 mainLoop();
 
-player.X += 1;
-
 function mainLoop() {
+  // Pre variable adjustments
+  player.X += player.Velocity_X;
+  player.Y += player.Velocity_Y;
+  // Logic
+  if (isLeft) player.Velocity_X = -3;
+  if (isRight) player.Velocity_X = 3;
+  if (!isLeft && !isRight) player.Velocity_X = 0;
+
+  // Post variable adjustments
+  // Rendering
   grphix.clearRect(0, 0, gameBoard.width, gameBoard.height); // refreshs screen so graphics dont pile on
   grphix.drawImage(player.Sprite, player.X, player.Y); // sets player to 'kinda bottom'
+
+  // player.X += 1; // cause char to move right
 
   setTimeout(mainLoop, 1000/60); // 60s refresh rate
 }
@@ -29,10 +53,6 @@ function Object(img, x, y) {
   this.Y = y;
   this.Previous_X;
   this.Previous_Y;
-  this.Velocity_X
-  this.Velocity_Y;
+  this.Velocity_X = 0;
+  this.Velocity_Y = 0;
 }
-// Pre variable adjustments
-// Logic
-// Post variable adjustments
-// Rendering
